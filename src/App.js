1953b40b9f6b44498d90f995b64f7e7fb53d66cd
maxwellburdette <maxbdevelops@gmail.com>
2021-05-06 19:07:10 -0500

    First Build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		let percentage = document.querySelector(".percentage");
+		let percent = document.querySelector(".percent");
+		navigator.getBattery().then((battery) => {
+			percentage.style.width = battery.level * 100 + "%";
+			percent.innerHTML = battery.level * 100 + "%";
+		});
+	}, []);
+
+	function handleClick() {
+		// let sec = document.querySelector(".sec");
+		// let toggle = document.querySelector(".toggle");
+		let sec = document.querySelector(".sec");
+		sec.classList.toggle("dark");
+	}
+	return (
+		<section className="sec">
+			<div className="toggle" onClick={handleClick}></div>
+			<div className="box">
+				<div className="content">
+					<h3>Battery</h3>
+					<div className="batteryShape">
+						<div className="level">
+							<div className="percentage"></div>
+						</div>
+					</div>
+
+					<div className="percent">50%</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default App;
